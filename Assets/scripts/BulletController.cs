17fb5dd282e33e17eece96546b5ae6e381f8b56f
noahdebont <23075@ma-web.nl>
2018-11-13 14:43:50 +0100

    project

---
+++ Assets/scripts/BulletController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public CircleCollider2D destructionCircle;
+    public static GroundController groundController;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+
+        if (coll.collider.tag == "Ground")
+        {
+            groundController.DestroyGround(destructionCircle);
+            Destroy(gameObject);
+        }
+    }
+}
